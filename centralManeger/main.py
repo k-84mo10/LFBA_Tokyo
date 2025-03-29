e76af836d41736353eb5efc29093b95e08a57867
k-84mo10 <k-84mo10@g.ecc.u-tokyo.ac.jp>
2025-03-29 15:13:49 +0900

    align for tokyo room

--- centralManeger/main.py
+++ centralManeger/main.py
@@ -129,8 +129,6 @@ def start_threads(serial_comm, camera):
         threading.Thread(target=save_images, args=(stop_event, mode_train, frame_queue, image_queue, camera, label_queue, start_time), daemon=True),
         threading.Thread(target=process_images, args=(stop_event, mode_train, frame_queue, write_queue, MODEL_PATH, CLASSES, classes_queue), daemon=True),
         threading.Thread(target=handle_received_data, daemon=True),
-        threading.Thread(target=monitor_folder, args=(stop_event, start_train, WATCH_DIR, DATASET_DIR, THRESHOLD, CHECK_INTERVAL), daemon=True),
-        threading.Thread(target=train_controller, args=(stop_event, start_train, BATCH_SIZE, EPOCHS, IMG_SIZE, LEARNING_RATE, DATASET_DIR, MODEL_DIR, MODEL_NAME, GPU, classes_queue, arch), daemon=True),
         threading.Thread(target=user_input_listener, daemon=True)
     ]
 
